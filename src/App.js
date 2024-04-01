53422bc23e8e041bb4645bd21becb277b14c0fd0
Aida Hashemi <aidahashemiam@gmail.com>
2024-04-02 00:58:37 +0330

    Add: basic styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import "./App.css";
 
-function App() {}
+function App() {
+  return (
+    <div className="app">
+      <h1>Magic Match</h1>
+      <button>New Game</button>
+    </div>
+  );
+}
 
 export default App;
