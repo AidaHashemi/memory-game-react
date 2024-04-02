e61e359ad907d7317fbf3723dc7f1016d5355b98
Aida Hashemi <aidahashemiam@gmail.com>
2024-04-02 22:06:46 +0330

    Handle : choices and compare two selected cards

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,10 +1,18 @@
 import "./SingleCard.css";
-const SingleCard = ({ card }) => {
+const SingleCard = ({ card, handleChoice }) => {
+  function handleClick() {
+    handleChoice(card);
+  }
   return (
     <div className="card">
       <div>
         <img className="front" src={card.src} alt="front card" />
-        <img className="back" src="/img/cover.png" alt="front card" />
+        <img
+          className="back"
+          src="/img/cover.png"
+          alt="front card"
+          onClick={handleClick}
+        />
       </div>
     </div>
   );
