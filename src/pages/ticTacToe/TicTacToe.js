fbd8c7ff62f13e6d2865ab0ad32da2f479a8888d
Aida Hashemi <aidahashemiam@gmail.com>
2024-05-13 23:58:01 +0330

    Create : some TicTacToe game components and create states for players and tiles

--- src/pages/ticTacToe/TicTacToe.js
+++ src/pages/ticTacToe/TicTacToe.js
@@ -1,7 +1,25 @@
-import React from "react";
+import { useState } from "react";
+import Board from "../../components/TicTacToGame/Board";
 
+import "./TicTacToe.css";
+
+const playerX = "X";
+const playerO = "O";
 const TicTacToe = () => {
-  return <div>tic tac to............</div>;
+  const [tiles, setTiles] = useState(Array(9).fill(null));
+  const [player, setPlayer] = useState(playerX);
+
+  const handleTileClick = (i) => {
+    console.log(i);
+  };
+  return (
+    <div className="tic-tac-to-container">
+      <div className="tic-tac-toe-content">
+        <h2>tic tac to</h2>
+        <Board tiles={tiles} onTileClick={handleTileClick} />
+      </div>
+    </div>
+  );
 };
 
 export default TicTacToe;
